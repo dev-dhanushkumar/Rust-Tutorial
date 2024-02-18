47b2b20167f5a2202683173f8b7283ed8d57e11d
dev-dhanushkumar <gunarakath2711@gmail.com>
2024-02-18 22:51:32 +0530

    processing...

---
+++ Algorithms/merge_sort.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let arr = vec![234,775,222,124,889,54,412,903,212];
+    let arr_size = arr.len();
+    
+    println!("Before Sorting: {:?}", arr);
+
+}
+
+fn marge_sort(&arr,arr_start: l, arr_len: r ) {
+    
+}
+
