4fca8fc7bdbaed8062ee67c3e5c11462f5a035df
dev-dhanushkumar <gunarakath2711@gmail.com>
2024-06-03 21:47:21 +0300

    update selection sort

--- Algorithms/Selection Sort/selection_sort.rs
+++ Algorithms/Selection Sort/selection_sort.rs
@@ -1,10 +1,11 @@
 fn main() {
-    let mut arr = vec![98,56,12,132,45,23,86,67,92,13];
+    let mut arr = vec![9,54,21,11,7];
     println!("Before Sorting: {:?}", arr);
+    let len = vector_length(&arr);
 
-    for i in 0..arr.len()-1 {
+    for i in 0..len-1 {
         let mut min_idx = i;
-        for j in i+1..arr.len() {
+        for j in i+1..len {
             if arr[j] < arr[min_idx] {
                 min_idx = j;
             }
@@ -13,7 +14,16 @@ fn main() {
         let temp = arr[i];
         arr[i] = arr[min_idx];
         arr[min_idx] = temp;
+        println!("Sorting Processing case {}: {:?}",i, arr);
     }
 
     println!("After Sorting: {:?}", arr);
+}
+
+fn vector_length<T>(v: &[T]) -> usize {
+    if v.is_empty() {
+        0
+    } else {
+        1 + vector_length(&v[1..])
+    }
 }
